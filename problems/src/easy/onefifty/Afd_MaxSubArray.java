943fa61b143a84fc3316c26d6d3d41edbe96447e
Hasaker <Arszrc@gmail.com>
2018-11-01 23:01:55 +0800

    38 53

---
+++ problems/src/easy/onefifty/Afd_MaxSubArray.java
@@ -0,0 +1,25 @@
+package easy.onefifty;
+
+public class Afd_MaxSubArray {
+
+    private static int maxSubArray(int[] nums) {
+        int maxScore = Integer.MIN_VALUE;
+        int now = 0;
+
+        for (int num : nums) {
+            if (num > maxScore) {
+                maxScore = num;
+            }
+            if (now + num > 0) {
+                now = now + num;
+                if (now > maxScore) {
+                    maxScore = now;
+                }
+            } else {
+                now = 0;
+            }
+        }
+
+        return maxScore;
+    }
+}
