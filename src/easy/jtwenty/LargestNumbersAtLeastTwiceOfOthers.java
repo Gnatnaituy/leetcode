42b4643723b1718839180439c36ddd00ab355e26
Hasaker <Arszrc@gmail.com>
2019-01-01 14:04:27 +0800

    748 754 762 766 771

--- src/easy/jtwenty/LargestNumbersAtLeastTwiceOfOthers.java
+++ src/easy/jtwenty/LargestNumbersAtLeastTwiceOfOthers.java
@@ -9,13 +9,11 @@ import java.util.Arrays;
 public class LargestNumbersAtLeastTwiceOfOthers {
     public int dominantIndex(int[] nums) {
         int res = 0;
-        if (nums.length == 1) 
-            res = 0;
+        if (nums.length == 1) res = 0;
         else {
             int[] cp = nums.clone();
             Arrays.sort(cp);
-            if (cp[cp.length - 1] < 2 * cp[cp.length - 2])
-                res = -1;
+            if (cp[cp.length - 1] < 2 * cp[cp.length - 2]) res = -1;
             else {
                 for (int i = 0; i < nums.length; i++) {
                     if (nums[i] == cp[cp.length - 1]) {
