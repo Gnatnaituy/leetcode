8cfd8d0686eb25f333fa28f60cda12f1825e4d77
Hasaker <Arszrc@gmail.com>
2019-01-10 16:11:15 +0800

    509 888 892

---
+++ src/easy/etwenty/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package easy.etwenty;
+
+/**
+ * 509 Fibonacci Number
+ */
+
+public class FibonacciNumber {
+
+    private int fib(int N) {
+        int a = 0, b = 1, temp;
+        for (int i = 2; i <= N; i++) {
+            temp = a + b;
+            a = b;
+            b = temp;
+        }
+        return N == 0 ? a : b;
+    }
+}
