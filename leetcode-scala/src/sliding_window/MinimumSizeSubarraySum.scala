ba6ddf5831b1e2f7aae42e7d6fa45d2e84411540
Hasaker <1758267482@qq.com>
2020-06-29 09:44:16 +0800

    Minimum Size Subarray Sum

---
+++ leetcode-scala/src/sliding_window/MinimumSizeSubarraySum.scala
@@ -0,0 +1,31 @@
+package sliding_window
+
+object MinimumSizeSubarraySum {
+
+  /**
+   * 滑动窗口
+   * 652ms 50.00%
+   * 52.8MB 100.00%
+   */
+  def minSubArrayLen(s: Int, nums: Array[Int]): Int = {
+    if (nums == null || nums.isEmpty) return 0
+
+    var (left, sum, len) = (0, 0, 0)
+
+    for (right <- nums.indices) {
+      sum += nums(right)
+      while (sum >= s) {
+        len = if (len == 0) right - left + 1 else Math.min(len, right - left + 1)
+        sum -= nums(left)
+        left += 1
+      }
+    }
+
+    len
+  }
+
+  def main(args: Array[String]): Unit = {
+    print(minSubArrayLen(5, Array(1, 2, 3 ,4, 5)))
+  }
+}
+
