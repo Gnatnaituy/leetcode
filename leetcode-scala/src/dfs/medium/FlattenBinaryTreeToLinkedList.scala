dfbd9d8713d6ed11c04900295e8e5e0ccfa6a6b9
Ravooo <1758267482@qq.com>
2020-07-19 23:59:12 +0800

    dfs: medium[1]

--- leetcode-scala/src/dfs/medium/FlattenBinaryTreeToLinkedList.scala
+++ leetcode-scala/src/dfs/medium/FlattenBinaryTreeToLinkedList.scala
@@ -40,4 +40,35 @@ object FlattenBinaryTreeToLinkedList {
             head = head.right
         }
     }
+
+    /**
+     * 652 ms   33.33%
+     * 50.9MB   100.00%
+     * 将左子树插入到右子树的地方
+     * 将原来的右子树接到左子树的最右边节点
+     * 考虑新的右子树的根节点，一直重复上边的过程，直到新的右子树为 null
+     * @param root
+     */
+    def flatten2(root: TreeNode): Unit = {
+        var cur = root
+
+        while (cur != null) {
+            // 如果左子树为null 直接跳过
+            if (cur.left == null) {
+                cur = cur.right
+            } else {
+                //找到左子树最右的节点
+                var pre = cur.left
+                while (pre.right != null) {
+                    pre = pre.right
+                }
+                // 将原来的右子树连接到左子树最右节点的右子树
+                pre.right = cur.right
+                // 将左子树挪到右子树
+                cur.right = cur.left
+                cur.left = null
+                cur = cur.right
+            }
+        }
+    }
 }
