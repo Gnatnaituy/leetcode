91f6bd6f6b6c26b4d28bc0da2132389475159deb
Ravooo <1758267482@qq.com>
2021-03-26 23:26:22 +0800

    15. 3Sum

---
+++ leetcode-java/src/classify/twopoints/ThreeSum.java
@@ -0,0 +1,92 @@
+package classify.twopoints;
+
+import java.util.*;
+
+/**
+ * @author yutiantang
+ * @create 2021/3/26 10:29 PM
+ */
+public class ThreeSum {
+
+    /**
+     * 超出时间限制
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        if (nums == null || nums.length < 3) {
+            return res;
+        }
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            if (nums[i] > 0) {
+                break;
+            }
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                if (nums[i] + nums[j] > 0) {
+                    break;
+                }
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (k > j + 1 && nums[k] == nums[k - 1]) {
+                        continue;
+                    }
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (sum > 0) {
+                        break;
+                    }
+                    if (sum == 0) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        break;
+                    }
+                }
+            }
+        }
+
+        return res;
+    }
+
+    /**
+     * 大佬解法1
+     * 26ms     49.49%
+     * 42.3MB   70.47%
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum2(int[] nums) {// 总时间复杂度：O(n^2)
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null || nums.length <= 2) return ans;
+
+        Arrays.sort(nums); // O(nlogn)
+
+        for (int i = 0; i < nums.length - 2; i++) { // O(n^2)
+            if (nums[i] > 0) break; // 第一个数大于 0，后面的数都比它大，肯定不成立了
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去掉重复情况
+            int target = -nums[i];
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    ans.add(new ArrayList<>(Arrays.asList(nums[i], nums[left], nums[right])));
+
+                    // 现在要增加 left，减小 right，但是不能重复，比如: [-2, -1, -1, -1, 3, 3, 3], i = 0, left = 1, right = 6, [-2, -1, 3] 的答案加入后，需要排除重复的 -1 和 3
+                    left++; right--; // 首先无论如何先要进行加减操作
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {  // nums[left] + nums[right] > target
+                    right--;
+                }
+            }
+        }
+        return ans;
+    }
+}
