7887e8d1577db8d0c9a9352219bb00a0eacde14a
Ravooo <1758267482@qq.com>
2021-03-29 22:12:15 +0800

    345. Reverse Vowels of a String

--- leetcode-java/src/classify/twopoints/ReverseVowelsOfAString.java
+++ leetcode-java/src/classify/twopoints/ReverseVowelsOfAString.java
@@ -32,4 +32,38 @@ public class ReverseVowelsOfAString {
 
         return new String(res);
     }
+
+    /**
+     * 3ms      89.11%
+     * 38.4MB   87.86
+     * @param s
+     * @return
+     */
+    public String reverseVowels2(String s) {
+        int left = 0, right = s.length() - 1;
+        char[] chars = s.toCharArray();
+        while (left < right) {
+            if (isVowel(chars[left]) && isVowel(chars[right])) {
+                char swap = chars[left];
+                chars[left] = chars[right];
+                chars[right] = swap;
+                left++;
+                right--;
+            } else {
+                if (!isVowel(chars[left])) {
+                    left++;
+                }
+                if (!isVowel(chars[right])) {
+                    right--;
+                }
+            }
+        }
+
+        return String.valueOf(chars);
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c =='U';
+    }
 }
