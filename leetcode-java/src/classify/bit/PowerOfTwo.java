b5509b276a14718cbabc5f58a869dac034a757a1
Gnatnaituy <1758267482@qq.com>
2021-05-07 09:45:51 +0800

    231. Power of Two

--- leetcode-java/src/classify/bit/PowerOfTwo.java
+++ leetcode-java/src/classify/bit/PowerOfTwo.java
@@ -2,10 +2,22 @@ package classify.bit;
 
 public class PowerOfTwo {
 
+    /**
+     * 2ms      7.19%
+     * 35.3MB   93.79%
+     * @param n
+     * @return
+     */
     public boolean isPowerOfTwo(int n) {
         return n > 0 && Integer.bitCount(n) == 1;
     }
 
+    /**
+     * 1ms      100.00%
+     * 35.5MB   58.01%
+     * @param n
+     * @return
+     */
     public boolean isPowerOfTwo2(int n) {
         return n > 0 && (n & (n - 1)) == 0;
     }
