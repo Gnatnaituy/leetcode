b3b862a7200120f91d3d1f59a65259d37903ba03
Gnatnaituy <1758267482@qq.com>
2021-10-24 15:21:07 +0800

    剑指 Offer 10- I. 斐波那契数列

--- leetcode-java/src/others/easy/etwenty/FibonacciNumber.java
+++ leetcode-java/src/others/easy/etwenty/FibonacciNumber.java
@@ -2,14 +2,21 @@ package others.easy.etwenty;
 
 /**
  * 509 Fibonacci Number
+ * 剑指 Offer 10- I. 斐波那契数列
+ * https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof/
  */
-
 public class FibonacciNumber {
 
+    /**
+     * 0ms      100.00%
+     * 35.3MB   29.81%
+     * @param N
+     * @return
+     */
     private int fib(int N) {
         int a = 0, b = 1, temp;
         for (int i = 2; i <= N; i++) {
-            temp = a + b;
+            temp = (a + b) % 1000000007;
             a = b;
             b = temp;
         }
