d1938a4016db3ae3172103414427266db200b88a
Gnatnaituy <1758267482@qq.com>
2021-10-25 17:58:31 +0800

    剑指 Offer 42. 连续子数组的最大和

--- leetcode-java/src/classify/dp/MaximumSubarray.java
+++ leetcode-java/src/classify/dp/MaximumSubarray.java
@@ -1,5 +1,9 @@
 package classify.dp;
 
+/**
+ * 剑指 Offer 42. 连续子数组的最大和
+ * https://leetcode-cn.com/problems/lian-xu-zi-shu-zu-de-zui-da-he-lcof/
+ */
 public class MaximumSubarray {
 
     /**
@@ -17,5 +21,24 @@ public class MaximumSubarray {
         }
 
         return finalMax;
+    }
+
+    /**
+     * 1ms      98.44%
+     * 45.1MB   26.70%
+     * @param nums
+     * @return
+     */
+    public int maxSubArray2(int[] nums) {
+        int fast = 0, res = Integer.MIN_VALUE, cur = 0;
+        while (fast < nums.length) {
+            cur += nums[fast++];
+            res = Math.max(res, cur);
+            if (cur < 0) {
+                cur = 0;
+            }
+        }
+
+        return res;
     }
 }
