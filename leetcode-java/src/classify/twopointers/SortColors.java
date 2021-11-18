e1a44e556841d959c69283e8e1a861a2dc756d5a
Gnatnaituy <1758267482@qq.com>
2021-11-18 15:33:23 +0800

    75. Sort Colors

--- leetcode-java/src/classify/twopointers/SortColors.java
+++ leetcode-java/src/classify/twopointers/SortColors.java
@@ -27,4 +27,37 @@ public class SortColors {
             }
         }
     }
+
+    /**
+     * 0ms 100.00%
+     * 34.5MB 100.00%
+     */
+    public void sortColors2(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int findZero = nums.length - 1;
+
+        while (left < right) {
+            if (nums[left] == 2) {
+                swap(nums, left, right);
+                right--;
+            } else if (nums[left] == 1) {
+                while (findZero > - 1 && nums[findZero] != 0) {
+                    findZero--;
+                }
+                if (findZero > left) {
+                    swap(nums, left, findZero);
+                }
+                left++;
+            } else {
+                left++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
 }
