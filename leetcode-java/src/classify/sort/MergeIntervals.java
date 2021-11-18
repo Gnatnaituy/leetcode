68336259b62f4e612ff0b9e4702ae0a6a472e017
Gnatnaituy <1758267482@qq.com>
2021-11-18 15:59:23 +0800

    56. Merge Intervals

--- leetcode-java/src/classify/sort/MergeIntervals.java
+++ leetcode-java/src/classify/sort/MergeIntervals.java
@@ -32,4 +32,33 @@ public class MergeIntervals {
 
         return Arrays.copyOfRange(res, 0, resIndex);
     }
+
+    /**
+     * 8ms      25.70%
+     * 41.2MB   14.60%
+     * @param intervals
+     * @return
+     */
+    public int[][] merge2(int[][] intervals) {
+        if (intervals.length < 2) {
+            return intervals;
+        }
+
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+
+        int[][] res = new int[intervals.length][2];
+        int slow = 0, fast = 1, index = 0;
+        while (fast < intervals.length) {
+            if (intervals[fast][0] > intervals[slow][1]) {
+                res[index++] = new int[]{intervals[slow][0], intervals[slow][1]};
+                slow = fast;
+            } else {
+                intervals[slow][1] = Math.max(intervals[slow][1], intervals[fast][1]);
+            }
+            fast++;
+        }
+        res[index++] = new int[]{intervals[slow][0], intervals[slow][1]};
+
+        return Arrays.copyOfRange(res, 0, index);
+    }
 }
