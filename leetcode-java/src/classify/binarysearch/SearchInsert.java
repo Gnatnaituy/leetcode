9d1c3cb1021c0f57eac710fcd95dfdcb2255cef2
Gnatnaituy <1758267482@qq.com>
2022-01-04 18:19:52 +0800

    35. Search Insert Position

--- leetcode-java/src/classify/binarysearch/SearchInsert.java
+++ leetcode-java/src/classify/binarysearch/SearchInsert.java
@@ -25,4 +25,28 @@ public class SearchInsert {
 
         return right;
     }
+
+    /**
+     * 0ms      100.oo%
+     * 37.9MB   82.47%
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int searchInsert2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return right + 1;
+    }
 }
