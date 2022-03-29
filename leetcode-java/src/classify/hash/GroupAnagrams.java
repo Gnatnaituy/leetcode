4e0475bcadec203d680c599a3b77d845026de1cd
Ravooo <1758267482@qq.com>
2022-03-29 19:48:16 +0800

    49. Group Anagrams

--- leetcode-java/src/classify/hash/GroupAnagrams.java
+++ leetcode-java/src/classify/hash/GroupAnagrams.java
@@ -1,24 +1,49 @@
 package classify.hash;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class GroupAnagrams {
 
     /**
-     * 19ms 90.05%
+     * 6ms      86.06%
+     * 44.2MB   48.25%
      */
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs.length == 0) return new ArrayList<>();
-        Map<String, List<String>> ans = new HashMap<>();
-
-        for (String s : strs) {
-            char[] ca = s.toCharArray();
-            Arrays.sort(ca);
-            String key = String.valueOf(ca);
-            if (!ans.containsKey(key)) ans.put(key, new ArrayList<>());
-            ans.get(key).add(s);
+        if (strs.length == 0) {
+            return new ArrayList<>();
         }
 
-        return new ArrayList<>(ans.values());
+        Map<String, List<String>> res = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = String.valueOf(chars);
+            if (!res.containsKey(key)) {
+                res.put(key, new ArrayList<>());
+            }
+            res.get(key).add(str);
+        }
+
+        return new ArrayList<>(res.values());
     }
+
+    /**
+     * 9ms      30.83%
+     * 43.8MB   69.89%
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams2(String[] strs) {
+        return new ArrayList<>(Arrays.stream(strs)
+                .collect(Collectors.groupingBy(
+                        str -> {
+                            char[] chars = str.toCharArray();
+                            Arrays.sort(chars);
+                            return new String(chars);
+                        }))
+                .values());
+    }
+
+
 }
