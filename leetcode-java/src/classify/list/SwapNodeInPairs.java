364d4081bc131d91bf2425f84c60717761829c1e
Ravooo <1758267482@qq.com>
2022-03-29 17:57:37 +0800

    24. Swap Nodes in Pairs

--- leetcode-java/src/classify/list/SwapNodeInPairs.java
+++ leetcode-java/src/classify/list/SwapNodeInPairs.java
@@ -5,22 +5,41 @@ import tools.ListNode;
 public class SwapNodeInPairs {
 
     /**
+     * 0ms      100.00%
+     * 36.1MB   37.73%
+     */
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode second = head.next;
+        ListNode third = head.next.next;
+        head.next = swapPairs(third);
+        second.next = head;
+
+        return second;
+    }
+
+    /**
      * 0ms 100.00%
      * 33.6MB 100.00%
      */
-    public ListNode swapPairs(ListNode head) {
-        ListNode fakeHead = new ListNode(1);
-        fakeHead.next = head;
-        ListNode cur = fakeHead;
+    public ListNode swapPairs2(ListNode head) {
+        ListNode dummy = new ListNode(1, head);
+        ListNode cur = dummy, first, second, third;
 
         while (cur.next != null && cur.next.next != null) {
-            ListNode temp = cur.next;
-            cur.next = cur.next.next;
-            temp.next = cur.next.next;
-            cur.next.next = temp;
+            first = cur.next;
+            second = first.next;
+            third = second.next;
+
+            cur.next = second;
+            second.next = first;
+            first.next = third;
+
             cur = cur.next.next;
         }
 
-        return fakeHead.next;
+        return dummy.next;
     }
 }
