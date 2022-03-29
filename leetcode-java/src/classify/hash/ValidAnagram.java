f79f929041d9120e423554a010365d221749d86a
Ravooo <1758267482@qq.com>
2022-03-29 18:22:18 +0800

    242. Valid Anagram

---
+++ leetcode-java/src/classify/hash/ValidAnagram.java
@@ -0,0 +1,76 @@
+package classify.hash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class ValidAnagram {
+
+    /**
+     * 2ms      78.17%
+     * 41.6MB   21.55%
+     */
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+
+        for (int i = 0; i < str1.length; i++) {
+            if(str1[i] != str2[i]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * 25ms     5.52%
+     * 42.1MB   7.45%
+     */
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Integer> sMap = new HashMap<>();
+        HashMap<Character, Integer> tMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return sMap.equals(tMap);
+    }
+
+    /**
+     * 4ms      37.58%
+     * 41.3MB   51.41%
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isAnagram3(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int count : counter) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
