c0a666920cd8e814d199e8955c2e7ccf578ac6ad
Ravooo <1758267482@qq.com>
2022-04-01 16:19:41 +0800

    1. Two Sum

--- leetcode-java/src/classify/twopointers/TwoSum.java
+++ leetcode-java/src/classify/twopointers/TwoSum.java
@@ -9,36 +9,9 @@ import java.util.*;
 public class TwoSum {
 
     /**
-     * 先排序 再使用双指针
-     * 人家要的是坐标 不是数值。。。。。。
-     * @param nums
-     * @param target
-     * @return
-     */
-    public int[] twoSum(int[] nums, int target) {
-        Arrays.sort(nums);
-        int left = 0, right = nums.length - 1;
-        while (left < right) {
-            int sum = nums[left] + nums[right];
-            if (sum == target) {
-                return new int[]{left, right};
-            } else if (sum < target) {
-                left++;
-            } else {
-                right--;
-            }
-        }
-
-        return null;
-    }
-
-    /**
-     * 使用Map储存 差值-坐标 键值对
      * 0ms      100.00%
      * 38.3MB   96.64MB
-     * @param nums
-     * @param target
-     * @return
+     * 使用Map储存 差值-坐标 键值对
      */
     public int[] twoSum2(int[] nums, int target) {
         Map<Integer, Integer> index = new HashMap<>();
