1ff2ff7b34b026bb597fe4c9fba1c5434d9de50b
Ravooo <1758267482@qq.com>
2022-06-28 18:13:03 +0800

    3. Longest Substring Without Repeating Characters

--- leetcode-java/src/classify/twopointers/LongestSubstringWithoutRepeatingCharacters.java
+++ leetcode-java/src/classify/twopointers/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,5 +1,6 @@
 package classify.twopointers;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -14,6 +15,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
     /**
      * 9ms      40.47%
      * 39.3MB   5.34%
+     *
      * @param s
      * @return
      */
@@ -46,26 +48,25 @@ public class LongestSubstringWithoutRepeatingCharacters {
 
     /**
      * 2ms大佬的解法
+     * 思路, 寻找两个相邻相同的字母的最大间距
+     * 如果字母只有一个,则是0到字母的距离
      * 2ms      99.89%
      * 38.3MB   75.28%
+     *
      * @param s
      * @return
      */
     public int lengthOfLongestSubstring2(String s) {
-        // 记录字符上一次出现的位置
-        int[] last = new int[128];
-        for(int i = 0; i < 128; i++) {
-            last[i] = -1;
-        }
-        int n = s.length();
+        int[] previous = new int[128];
+        Arrays.fill(previous, -1);
 
         int res = 0;
         int start = 0; // 窗口开始位置
-        for(int i = 0; i < n; i++) {
-            int index = s.charAt(i);
-            start = Math.max(start, last[index] + 1);
-            res   = Math.max(res, i - start + 1);
-            last[index] = i;
+        for (int i = 0; i < s.length(); i++) {
+            int c = s.charAt(i);
+            start = Math.max(start, previous[c] + 1);
+            res = Math.max(res, i - start + 1);
+            previous[c] = i;
         }
 
         return res;
@@ -75,6 +76,7 @@ public class LongestSubstringWithoutRepeatingCharacters {
      * 2ms      99.89%
      * 38.1MB   94.55%
      * 快慢指针 空格真坑人 还以为全是小写字母
+     *
      * @param s
      * @return
      */
