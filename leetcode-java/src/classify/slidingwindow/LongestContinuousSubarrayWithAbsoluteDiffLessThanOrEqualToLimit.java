fa3613b02ddab08e1d190467ab870365422617c4
Ravooo <1758267482@qq.com>
2022-07-04 16:36:17 +0800

    589. N-ary Tree Preorder Traversal

---
+++ leetcode-java/src/classify/slidingwindow/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -0,0 +1,27 @@
+package classify.slidingwindow;
+
+public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
+
+    public static int longestSubarray(int[] nums, int limit) {
+        int slow = 0, fast = 0, min = nums[0] - limit, max = nums[0] + limit, res = 0;
+        while (fast < nums.length) {
+            if (nums[fast] > max || nums[fast] < min) {
+                max = Math.max(max, nums[slow] + limit);
+                min = Math.min(min, nums[slow] - limit);
+                slow++;
+            } else {
+                max = Math.min(max, nums[fast] + limit);
+                min = Math.max(min, nums[fast] - limit);
+                res = Math.max(res, fast - slow + 1);
+                fast++;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(longestSubarray(new int[]{8,2,4,7}, 4));
+        System.out.println((int) ((double) 35 / 235 * 100));
+    }
+}
