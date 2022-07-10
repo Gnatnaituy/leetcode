2e972b585871e5601d922b5a40bc25ae1f94fad4
Ravooo <1758267482@qq.com>
2022-07-11 02:19:41 +0800

    23. Merge k Sorted Lists

--- leetcode-java/src/classify/list/MergeKSortedLists.java
+++ leetcode-java/src/classify/list/MergeKSortedLists.java
@@ -6,25 +6,54 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.stream.Collectors;
 
 public class MergeKSortedLists {
 
     /**
-     * 100ms    25.90%
-     * 40.2MB   33.39%
+     * 100ms 25.90%
+     * 40.2MB 33.39%
      *
      * @param lists
      * @return ListNode
      */
     public ListNode mergeKLists(ListNode[] lists) {
-        if (lists.length == 0) return null;
+        if (lists.length == 0)
+            return null;
 
         for (int i = lists.length - 1; i > 0; i--) {
             lists[i - 1] = mergeTwoLists(lists[i], lists[i - 1]);
         }
 
         return lists[0];
+    }
+
+    /**
+     * 相较于上面的方法，避免了一条ListNode过长的情况，所以更快
+     * 1ms      100.00%
+     * 43.2MB   69.13%
+     */
+    public ListNode mergeKLists4(ListNode[] lists) {
+        if (lists.length ==0) {
+            return null;
+        }
+
+        return mergeLinkedList(lists, 0, lists.length - 1);
+    }
+
+    private ListNode mergeLinkedList(ListNode[] lists, int start, int end) {
+        if (end == start) {
+            return lists[start];
+        }
+        if (end - start == 1) {
+            return mergeTwoLists(lists[start], lists[end]);
+        }
+        int mid = (start + end) / 2;
+        ListNode leftRes = mergeLinkedList(lists, start, mid);
+        ListNode rightRes = mergeLinkedList(lists, mid + 1, end);
+
+        return mergeTwoLists(leftRes, rightRes);
     }
 
     private ListNode mergeTwoLists(ListNode list1, ListNode list2) {
@@ -48,8 +77,9 @@ public class MergeKSortedLists {
     }
 
     /**
-     * 10ms     29.52%
-     * 39.9MB   83.22%
+     * 10ms 29.52%
+     * 39.9MB 83.22%
+     * 
      * @param lists
      * @return
      */
@@ -62,12 +92,10 @@ public class MergeKSortedLists {
             }
         }
 
-        ListNode res = new ListNode(0);
-        ListNode cur = res;
+        ListNode res = new ListNode(0), cur = res;
         List<Map.Entry<Integer, Integer>> entries = numCount.entrySet().stream()
                 .sorted(Comparator.comparingInt(Map.Entry::getKey))
                 .collect(Collectors.toList());
-
 
         for (Map.Entry<Integer, Integer> entry : entries) {
             int num = entry.getKey();
@@ -79,5 +107,36 @@ public class MergeKSortedLists {
         }
 
         return res.next;
+    }
+
+    /**
+     * 小顶堆辅助
+     * 4ms 70.57%
+     * 43.2MB 64.50%
+     */
+    public ListNode mergeKLists3(ListNode[] lists) {
+        if (lists.length == 0) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(-1), cur = dummy;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, (o1, o2) -> (o1.val - o2.val));
+
+        for (ListNode head : lists) {
+            if (head != null) {
+                queue.add(head);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            ListNode node = queue.poll();
+            cur.next = node;
+            if (node.next != null) {
+                queue.add(node.next);
+            }
+            cur = cur.next;
+        }
+
+        return dummy.next;
     }
 }
