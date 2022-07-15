3f22eb9407f7598e97a9286682d9e3cf11871f8a
Ravooo <1758267482@qq.com>
2022-07-15 15:11:08 +0800

    167. Two Sum II - Input Array Is Sorted

--- leetcode-java/src/classify/twopointers/TwoSumII.java
+++ leetcode-java/src/classify/twopointers/TwoSumII.java
@@ -7,8 +7,7 @@ public class TwoSumII {
      * 36.5MB 67.26%
      */
     public int[] twoSum(int[] numbers, int target) {
-        int left = 0;
-        int right = numbers.length - 1;
+        int left = 0, right = numbers.length - 1;
 
         while (left < right) {
             int sum = numbers[left] + numbers[right];
@@ -18,6 +17,26 @@ public class TwoSumII {
                 left++;
             } else {
                 right--;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 1ms      98.92%
+     * 43.8MB   90.26%
+     */
+    public int[] twoSum2(int[] numbers, int target) {
+        if (numbers.length < 2) return null;
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (numbers[i] + numbers[j] < target)
+                    continue;
+                if (numbers[i] + numbers[j] == target)
+                    return new int[]{j + 1, i + 1};
+                else break;
             }
         }
 
