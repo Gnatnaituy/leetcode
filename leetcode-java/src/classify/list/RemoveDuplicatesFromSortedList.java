120790a80627d4fc7d96162a1675e7d797e251ca
Ravooo <1758267482@qq.com>
2022-07-15 15:17:08 +0800

    83. Remove Duplicates from Sorted List

--- leetcode-java/src/classify/list/RemoveDuplicatesFromSortedList.java
+++ leetcode-java/src/classify/list/RemoveDuplicatesFromSortedList.java
@@ -23,4 +23,22 @@ public class RemoveDuplicatesFromSortedList {
 
         return null;
     }
+
+    /**
+     * 0ms      100.00%
+     * 41MB     54.09%
+     */
+    public ListNode deleteDuplicates2(ListNode head) {
+        ListNode cur = head;
+
+        while (cur != null && cur.next != null) {
+            if (cur.next.val == cur.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return head;
+    }
 }
