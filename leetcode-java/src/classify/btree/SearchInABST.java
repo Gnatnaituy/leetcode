ca4f55079f709c86e7349657bcb1742c18dc2f1a
Ravooo <1758267482@qq.com>
2022-07-24 00:36:27 +0800

    700. Search in a Binary Search Tree

--- leetcode-java/src/classify/btree/SearchInABST.java
+++ leetcode-java/src/classify/btree/SearchInABST.java
@@ -5,14 +5,36 @@ import tools.TreeNode;
 public class SearchInABST {
 
     /**
-     * 0ms 100.00%
-     * 39.8MB 94.80%
+     * Recursion
+     * 0ms      100.00%
+     * 39.8MB   94.80%
      */
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) return null;
+        if (root == null)
+            return null;
 
-        if (root.val == val) return root;
-        if (root.val > val) return searchBST(root.left, val);
-        else return searchBST(root.right, val);
+        if (root.val == val)
+            return root;
+        if (root.val > val)
+            return searchBST(root.left, val);
+        else
+            return searchBST(root.right, val);
+    }
+
+    /**
+     * Iteration
+     * 0ms      100.00%
+     * 42MB     24.89%
+     */
+    public TreeNode searchBST2(TreeNode root, int val) {
+        while (root != null) {
+            if (root.val == val) {
+                return root;
+            } else {
+                root = root.val < val ? root.right : root.left;
+            }
+        }
+
+        return null;
     }
 }
