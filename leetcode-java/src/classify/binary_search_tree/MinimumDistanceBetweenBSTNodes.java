9e7c229e3c6134228b5ed6e4f55c07ec2873bd41
Ravooo <1758267482@qq.com>
2022-07-27 18:43:24 +0800

    783. Minimum Distance Between BST Nodes

---
+++ leetcode-java/src/classify/binary_search_tree/MinimumDistanceBetweenBSTNodes.java
@@ -0,0 +1,30 @@
+package classify.binary_search_tree;
+
+import tools.TreeNode;
+
+public class MinimumDistanceBetweenBSTNodes {
+    int res = Integer.MAX_VALUE;
+    int pre = Integer.MAX_VALUE;
+
+    /**
+     * 0ms      100.00%
+     * 39.5MB   5.53%
+     */
+    public int minDiffInBST(TreeNode root) {
+        inOrder(root);
+        return res;
+    }
+
+    private void inOrder(TreeNode root) {
+        if (root != null) {
+            inOrder(root.left);
+            if (pre == Integer.MAX_VALUE) {
+                pre = root.val;
+            } else {
+                res = Math.min(res, root.val - pre);
+                pre = root.val;
+            }
+            inOrder(root.right);
+        }
+    }
+}
