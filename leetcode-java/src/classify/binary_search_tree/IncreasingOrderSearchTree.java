97ce294adeb7d7a85408abc72954336d965b206f
Ravooo <1758267482@qq.com>
2022-07-29 17:04:10 +0800

    897. Increasing Order Search Tree

---
+++ leetcode-java/src/classify/binary_search_tree/IncreasingOrderSearchTree.java
@@ -0,0 +1,65 @@
+package classify.binary_search_tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tools.TreeNode;
+
+public class IncreasingOrderSearchTree {
+
+    /**
+     * 0ms      100.00%
+     * 38.7MB   98.46%
+     */
+    private List<Integer> elements = new ArrayList<>();
+    public TreeNode increasingBST(TreeNode root) {
+        inOrder(root);
+        TreeNode dummy = new TreeNode(0);
+        TreeNode cur = dummy;
+        for (int element : elements) {
+            TreeNode node = new TreeNode(element);
+            cur.right = node;
+            cur = cur.right;
+        }
+
+        return dummy.right;
+    }
+
+    private void inOrder(TreeNode node) {
+        if (node.left != null) {
+            inOrder(node.left);
+        }
+        elements.add(node.val);
+        if (node.right != null) {
+            inOrder(node.right);
+        }
+    }
+
+
+    /**
+     * 0ms      100.00%
+     * 39.2MB   30.52%
+     */
+    private TreeNode curNode;
+    public TreeNode increasingBST2(TreeNode root) {
+        TreeNode dummy = new TreeNode(0);
+        curNode = dummy;
+        inorder(root);
+
+        return dummy.right;
+    }
+
+    public void inorder(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inorder(node.left);
+
+        curNode.right = node;
+        node.left = null;
+        curNode = curNode.right;
+
+        inorder(node.right);
+    }
+}
