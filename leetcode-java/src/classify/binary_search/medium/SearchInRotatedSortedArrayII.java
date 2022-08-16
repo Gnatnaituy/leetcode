27c63b51a543d96fb5773b713559216517975d80
Ravooo <1758267482@qq.com>
2022-08-16 19:58:14 +0800

    81. Search in Rotated Sorted Array II

--- leetcode-java/src/classify/binary_search/medium/SearchInRotatedSortedArrayII.java
+++ leetcode-java/src/classify/binary_search/medium/SearchInRotatedSortedArrayII.java
@@ -31,4 +31,38 @@ public class SearchInRotatedSortedArrayII {
                 : search(nums, target, mid + 1, right);
         }
     }
+
+    /**
+     * 0ms      100.00%
+     * 41.6MB   5.05%
+     */
+    public boolean search2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1, middle;
+
+        while (left <= right) {
+            middle = left + (right - left) / 2;
+
+            if (nums[middle] == target) return true;
+
+            if (nums[middle] > nums[left] || nums[middle] > nums[right]) {
+                // For this condition we know left side is sorted and right side is unsorted
+                if (nums[left] <= target && target <= nums[middle]) {
+                    right = middle - 1;
+                } else {
+                    left = middle + 1;
+                }
+            } else if (nums[middle] < nums[right] || nums[middle] < nums[left]) {
+                // For this condition we know right side is sorted and left side is unsorted
+                if (nums[middle] <= target && target <= nums[right]) {
+                    left = middle + 1;
+                } else {
+                    right = middle - 1;
+                }
+            } else {
+                left++;  // right-- is the same because nums[left] == nums[middle] == nums[right]
+            }
+        }
+
+        return false;
+    }
 }
