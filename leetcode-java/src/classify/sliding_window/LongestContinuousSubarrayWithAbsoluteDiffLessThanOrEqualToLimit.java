57f235b30641eb1658da815d6ba637189924a728
Ravooo <1758267482@qq.com>
2022-09-06 19:03:18 +0800

    1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit

--- leetcode-java/src/classify/sliding_window/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
+++ leetcode-java/src/classify/sliding_window/LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit.java
@@ -1,27 +1,38 @@
 package classify.sliding_window;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class LongestContinuousSubarrayWithAbsoluteDiffLessThanOrEqualToLimit {
 
-    public static int longestSubarray(int[] nums, int limit) {
-        int slow = 0, fast = 0, min = nums[0] - limit, max = nums[0] + limit, res = 0;
-        while (fast < nums.length) {
-            if (nums[fast] > max || nums[fast] < min) {
-                max = Math.max(max, nums[slow] + limit);
-                min = Math.min(min, nums[slow] - limit);
-                slow++;
-            } else {
-                max = Math.min(max, nums[fast] + limit);
-                min = Math.max(min, nums[fast] - limit);
-                res = Math.max(res, fast - slow + 1);
-                fast++;
+    /**
+     * 27ms     87.75%
+     * 49.1MB   96.08%
+     * 滑动窗口-双指针 宫水三叶
+     */
+    public int longestSubarray(int[] nums, int limit) {
+        Deque<Integer> max = new ArrayDeque<>(), min = new ArrayDeque<>();
+        int res = Integer.MIN_VALUE;
+        
+        for (int slow = 0, fast = 0; fast < nums.length; fast++) {
+            while (!max.isEmpty() && nums[fast] >= nums[max.peekLast()]) {
+                max.pollLast();
             }
+            max.addLast(fast);
+
+            while (!min.isEmpty() && nums[fast] <= nums[min.peekLast()]) {
+                min.pollLast();
+            }
+            min.addLast(fast);
+
+            while (Math.abs(nums[max.peekFirst()] - nums[min.peekFirst()]) > limit) {
+                slow++;
+                if (max.peekFirst() < slow) max.pollFirst();
+                if (min.peekFirst() < slow) min.pollFirst();
+            }
+            res = Math.max(res, fast - slow + 1);
         }
 
         return res;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(longestSubarray(new int[]{8,2,4,7}, 4));
-        System.out.println((int) ((double) 35 / 235 * 100));
     }
 }
