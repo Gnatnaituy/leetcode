2771f67cf2bf9b61838d14d4b0600d66b5441f39
ravooo <1758267482@qq.com>
2022-10-12 21:56:04 +0800

    62. Unique Paths

--- leetcode-java/src/classify/dp/UniquePaths.java
+++ leetcode-java/src/classify/dp/UniquePaths.java
@@ -5,10 +5,32 @@ import java.util.Arrays;
 public class UniquePaths {
 
     /**
-     * 0ms 100.00%
-     * 31.8MB 100.00%
+     * 0ms      100.00%
+     * 38.1MB   85.67%
      */
     public int uniquePaths(int m, int n) {
+        int[][] road = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            road[i][0] = 1;
+        }
+        for (int j = 0; j < m; j++) {
+            road[0][j] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                road[i][j] = road[i - 1][j] + road[i][j - 1];
+            }
+        }
+
+        return road[n - 1][m - 1];
+    }
+
+    /**
+     * 0ms      100.00%
+     * 31.8MB   100.00%
+     */
+    public int uniquePaths2(int m, int n) {
         if (m == 1 || n == 1) return 1;
 
         int[] dp = new int[m];
