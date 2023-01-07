f21cef07d89a4cab438dd4f2fa5a52b8496dc31d
Ravooo <1758267482@qq.com>
2023-01-08 00:22:51 +0800

    又好久刷题了 我的人生就是这样不断的半途而废吗

--- leetcode-java/src/classify/array/SetMatrixZeroes.java
+++ leetcode-java/src/classify/array/SetMatrixZeroes.java
@@ -1,4 +1,4 @@
-package others.completefifties.l0000;
+package classify.array;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -6,7 +6,9 @@ import java.util.Set;
 public class SetMatrixZeroes {
 
     /**
-     * 5ms 26.81%
+     * 1ms      37.12%      O(mn)
+     * 43.3MB   13.34%      O(m+n)
+     * 使用额外数组记录零值位置，遍历零值将所在行和列全部置为0
      */
     public void setZeroes(int[][] matrix) {
         Set<int[]> set = new HashSet<>();
@@ -33,56 +35,41 @@ public class SetMatrixZeroes {
     }
 
     /**
-     * 1ms
+     * 0ms      100.00%     O(mn)
+     * 43.4MB   19.91%      O(1)
+     * 原地算法
      */
-    public void setZeroes2(int[][] matrix) {
-        boolean rowFlag = false;
-        //判断首行
-        for (int i = 0; i < matrix[0].length; i++) {
-            if (matrix[0][i] == 0) {
-                rowFlag = true;
-                break;
-            }
-        }
+    public void setZeroes1(int[][] matrix) {
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
 
-        boolean colFlag = false;
         for (int i = 0; i < matrix.length; i++) {
-            if (matrix[i][0] == 0) {
-                colFlag = true;
-                break;
-            }
-        }
-
-        for (int i = 1; i < matrix.length; i++) {
-            for (int j = 1; j < matrix[0].length; j++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
+                    if (i == 0) firstRowZero = true;
+                    if (j == 0) firstColZero = true;
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
             }
         }
 
-        for (int i = 1; i < matrix[0].length; i++) {
-            if (matrix[0][i] == 0) {
-                for (int j = 0; j < matrix.length; j++) {
-                    matrix[j][i] = 0;
-                }
-            }
-        }
-
+        // 因为依赖于第一行和第一列的0值，所以不能修改
         for (int i = 1; i < matrix.length; i++) {
-            if (matrix[i][0] == 0) {
-                for (int j = 0; j < matrix[0].length; j++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
-        if (rowFlag) {
+        
+        if (firstRowZero) {
             for (int i = 0; i < matrix[0].length; i++) {
                 matrix[0][i] = 0;
             }
         }
-        if (colFlag) {
+
+        if (firstColZero) {
             for (int i = 0; i < matrix.length; i++) {
                 matrix[i][0] = 0;
             }
