4f93ba0a8525e29b2fb743ce2121e88e55e7e7d2
Ravooo <1758267482@qq.com>
2023-01-10 21:59:29 +0800

    我的智商也就这样了吧

--- leetcode-java/src/classify/tree/PathSumIII.java
+++ leetcode-java/src/classify/tree/PathSumIII.java
@@ -2,34 +2,56 @@ package classify.tree;
 
 import tools.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PathSumIII {
 
     /**
-     * 11ms 63.97%
-     * 40.8MB 46.00%
+     * 24ms     10.56%%     O(n^2)
+     * 41.4MB   26.46%      O(n)
+     * DFS
      */
     public int pathSum(TreeNode root, int targetSum) {
-        return (int) pathSumFrom(root, targetSum);
+        if (root == null) return 0;
+
+        return pathSumFrom(root, targetSum)
+                + pathSum(root.left, targetSum)
+                + pathSum(root.right, targetSum);
     }
 
-    private long pathSumFrom(TreeNode node, long targetSum) {
-        if (node == null) return 0L;
+    private int pathSumFrom(TreeNode node, long targetSum) {
+        if (node == null) return 0;
 
-        long cur = node.val == targetSum ? 1L : 0L;
-        long left = pathSumFrom(node.left, targetSum - node.val);
-        long right = pathSumFrom(node.right, targetSum - node.val);
-
+        int cur = node.val == targetSum ? 1 : 0;
+        int left = pathSumFrom(node.left, targetSum - node.val);
+        int right = pathSumFrom(node.right, targetSum - node.val);
         return cur + left + right;
     }
 
-    public static void main(String[] args) {
-        TreeNode node = new TreeNode(1000000000);
-        node.left = new TreeNode(1000000000);
-        node.left.left = new TreeNode(294967296);
-        node.left.left.left = new TreeNode(1000000000);
-        node.left.left.left.left = new TreeNode(1000000000);
-        node.left.left.left.left.left = new TreeNode(1000000000);
+    /**
+     * 2ms     100.00%     O(n)
+     * 41.1MB  62.59%      O(n)
+     * Prefix Sum
+     */
+    public int pathSum2(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefix = new HashMap<Long, Integer>();
+        prefix.put(0L, 1);
 
-        System.out.println(new PathSumIII().pathSum(node, 0));
+        return dfs(root, prefix, 0, targetSum);
+    }
+
+    public int dfs(TreeNode root, Map<Long, Integer> prefix, long curr, int targetSum) {
+        if (root == null) return 0;
+
+        curr += root.val;
+
+        int res = prefix.getOrDefault(curr - targetSum, 0);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) + 1);
+        res += dfs(root.left, prefix, curr, targetSum);
+        res += dfs(root.right, prefix, curr, targetSum);
+        prefix.put(curr, prefix.getOrDefault(curr, 0) - 1);
+
+        return res;
     }
 }
