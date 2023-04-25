291bb9c70c0481c479d34cbab0a456f4b747e66d
Ravooo <1758267482@qq.com>
2023-04-25 17:09:18 +0800

    最近比较闲  touching fish

--- leetcode-python/subjects/424.替换后的最长重复字符.py
+++ leetcode-python/subjects/424.替换后的最长重复字符.py
@@ -11,27 +11,23 @@ from typing import List
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         max_len: int = 0
-        str_len: int = len(s)
         fast: int = 0
         slow: int = 0
-        char_distinct: int = 0
-        char_count: List[int] = [26]
+        char_num: List[int] = [0] * 26
 
-        while fast < str_len:
-            if (char_count[ord(s[fast]) - 65] == 0):
-                char_count[ord(s[fast]) - 65] = 1
-                char_distinct += 1
-            if (char_distinct > k):
-                while char_count[ord(s[slow])] != 0:
-                    char_count[ord(s[slow]) - 65] -= 1
-                    slow += 1
-                char_distinct -= 1
-            max_len = max(max_len, fast - slow + 1)
+        while fast < len(s):
+            char_num[ord(s[fast]) - 65] += 1
+            max_len = max(max_len, char_num[ord(s[fast]) - 65])
+            if fast - slow + 1 - max_len > k:
+                char_num[ord(s[slow]) - 65] -= 1
+                slow += 1
             fast += 1
+            print('slow:', slow, 'fast:', fast)
 
-        return max_len
+        return fast - slow
 
 # @lc code=end
 
 if __name__ == '__main__':
-    print(ord('A'))
+    so = Solution()
+    print(so.characterReplacement('ABABAAAB', 1))
